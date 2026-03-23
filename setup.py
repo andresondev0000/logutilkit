82af80fa4561669e293ab9417086d8878b56b315
shiningup1996@gmail.com <cointech0000@gmail.com>
2026-03-23 15:09:11 -0700

    version patch

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setuptools.setup(
     name="logutilkit",
-    version="0.52",
-    author="Tinywan",
-    author_email="756684177@qq.com",
+    version="1.0.1",
+    author="andresondev0000",
+    author_email="shiningup1996@gmail.com",
     description="A small log package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Tinywan/logutilkit",
+    url="https://github.com/andresondev0000/logutilkit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
